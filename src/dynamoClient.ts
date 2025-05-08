2b354da9f5c17441c8781db63ece10a97ad57130
asasmith <smith.asa.la@gmail.com>
2025-05-08 13:52:38 -0400

    chore: Add aws region

--- src/dynamoClient.ts
+++ src/dynamoClient.ts
@@ -1,3 +1,7 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
-export const client = new DynamoDBClient({});
+import 'dotenv/config';
+
+const AWS_REGION = process.env.AWS_REGION;
+
+export const client = new DynamoDBClient({ region: AWS_REGION || 'us-east-1' });
