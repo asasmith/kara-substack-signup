6d6f3276f8a179e91575064fe8c1004ee02b2666
asasmith <smith.asa.la@gmail.com>
2025-05-09 00:20:04 -0400

    chore: Add control flow for email processing

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import { emailSignup } from './emailSignup';
 (async () => {
     const emails = await fetchUnsubscribedEmails();
 
+    if (!emails.length) {
+        console.log('no emails');
+        return;
+    }
+
     for (const email of emails) {
         await emailSignup(email);
     }
