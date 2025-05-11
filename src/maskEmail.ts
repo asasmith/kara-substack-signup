e88a6de884fa20908f47a4342b6e0dba23102f89
asasmith <smith.asa.la@gmail.com>
2025-05-11 10:04:26 -0400

    chore: Mask domain name

--- src/maskEmail.ts
+++ src/maskEmail.ts
@@ -1,5 +1,6 @@
 export function maskEmail(email: string): string {
     const [local = '', domain = ''] = email.split('@');
+    const tld = domain.split('.')[1];
     const maskedLocal = local.length <= 3 ? '***' : local.slice(0, 3) + '***';
-    return `${maskedLocal}@${domain}`;
+    return `${maskedLocal}@***.${tld}`;
 }
--- src/tests/maskEmail.test.ts
+++ src/tests/maskEmail.test.ts
@@ -3,5 +3,5 @@ import { maskEmail } from '../maskEmail';
 test('should mask email', function () {
     const maskedEmail = maskEmail('test@test.com');
 
-    expect(maskedEmail).toEqual('tes***@test.com');
+    expect(maskedEmail).toEqual('tes***@***.com');
 });
